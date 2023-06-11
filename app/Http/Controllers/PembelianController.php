3a15f8731c63f4a19228d7e889c9d816f07439d9
acyandika <acyandika2@gmail.com>
2023-06-12 02:08:56 +0700

    today

--- app/Http/Controllers/PembelianController.php
+++ app/Http/Controllers/PembelianController.php
@@ -2,12 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Facades\Cart;
-use App\Models\Pembelian;
 use App\Models\Product;
-use Illuminate\Http\Request;
 use App\Models\KategoriProduct;
-use App\Facades\Cart as FacadesCart;
 
 class PembelianController extends Controller
 {
@@ -17,32 +13,4 @@ class PembelianController extends Controller
         $kategori = KategoriProduct::all();
         return view('pembelian.index', compact('product', 'kategori'));
     }
-
-    public function addToCart($id)
-    {
-        $product = Product::find($id);
-        Cart::add($product);
-        $this->emit('addToCart');
-
-        // dd($cart['product']);
-        // dd(Cart::get()['product']);
-    }
-
-    public $cart;
-    public function mount()
-    {
-        $this->cart = FacadesCart::get();
-    }
-
-    // public $cartTotal = 0;
-
-    // public function mount()
-    // {
-    //     $this->updateCartTotal();
-    // }
-
-    // public function updateCartTotal()
-    // {
-    //     $this->cartTotal = count(Cart::get()['product']);
-    // }
 }
